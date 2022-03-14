a0f1fb441cb943b8f18e6adf2e0925d7cbb6e5f7
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-03-14 16:37:14 -0300

    :sparkles: Create the page of Praia dos Ingleses
    #

--- scripts/listenerClick.js
+++ scripts/listenerClick.js
@@ -3,24 +3,28 @@ window.onload = ()=>{
     const componentLagoDaConceicao = document.querySelectorAll('.local2');
     const componentPraiaDoCampeche = document.querySelectorAll('.local3');
     const componentExpoTattoFloripa = document.querySelectorAll('.local4');
+    const componentPraiaDosIngleses = document.querySelector('.contrast-img');
     componentDoceECompainha.forEach(el => {
-        el.addEventListener('click',(event)=>{
+        el.addEventListener('click',()=>{
             window.location.href=`/locals/doceECompainha.html`;
         });
     });
     componentLagoDaConceicao.forEach(el => {
-        el.addEventListener('click',(event)=>{
+        el.addEventListener('click',()=>{
             window.location.href=`/locals/lagoDaConceicao.html`;
         });
     });
     componentPraiaDoCampeche.forEach(el => {
-        el.addEventListener('click',(event)=>{
+        el.addEventListener('click',()=>{
             window.location.href=`/locals/praiaDoCampeche.html`;
         });
     });
     componentExpoTattoFloripa.forEach(el =>{
-        el.addEventListener('click',(event)=>{
+        el.addEventListener('click',()=>{
             window.location.href=`/locals/expoTatto.html`;
         });
     });
+    componentPraiaDosIngleses.addEventListener('click',()=>{
+        window.location.href=`/locals/praiaDosIngleses.html`;
+    })
 }
