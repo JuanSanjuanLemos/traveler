2ca014743f8f3ca6cbf754e8b96330778972724b
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-03-15 16:38:47 -0300

    :construction: Create the window-new-rating
    #

--- scripts/window-rating.js
+++ scripts/window-rating.js
@@ -1,8 +1,16 @@
 const windowRating = document.querySelector('.window-all-ratings');
+const windowNewRating = document.querySelector('.window-new-rating');
 const openWindowRating = document.querySelector('.-view');
+const openWindowNewRating = document.querySelector('.-new');
 function closeWindow(){
     windowRating.style.display = "none";
 }
+function closeWindowNew(){
+    windowNewRating.style.display = 'none';
+}
 openWindowRating.addEventListener('click',()=>{
     windowRating.style.display = 'flex';
+})
+openWindowNewRating.addEventListener('click',()=>{
+    windowNewRating.style.display = 'flex';
 })
